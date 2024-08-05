b10612c692a3ff889fcfbec159f422568eae7505
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-08-05 23:09:20 +0200

    now exporting the keypoints

--- hand_recognition.py
+++ hand_recognition.py
@@ -125,7 +125,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 image, results = mediapipe_detection(frame, holistic)  # Process the frame
                 draw_landmarks(image, results)  # Draw landmarks on the frame
                 
-                if frame_num = 0:
+                if frame_num == 0:
                     cv2.putText(image, "starting collection", (120,200),
                                 cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA )
                     cv2.putText(image, f"collecting frames for {action} video number {sequence}", (120,200),
@@ -135,7 +135,11 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 else:
                     cv2.putText(image, "starting collection", (120,200),
                                 cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA )
-        cv2.imshow("feed", image)  # Show the image with landmarks
+                    
+                    keypoints = extract_keypoints(results)
+                    npy_path = os.path.join(Data_path, action, str(sequence), str(frame_num))
+                    np.save(npy_path, keypoints)
+                    cv2.imshow("feed", image)  # Show the image with landmarks
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
