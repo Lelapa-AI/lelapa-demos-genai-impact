3541ce067575472570d7a101913f9af0f7f1de77
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-08-06 00:10:32 +0200

    training section of code partially done

--- hand_recognition.py
+++ hand_recognition.py
@@ -28,21 +28,6 @@ actions = np.array(["hello", "thanks", "IloveYou"])
 no_sequence = 30
 sequence_length = 30
 
-label_map = {label:num for num, label in enumerate(actions)}
-sequences, labels = [], []
-for action in actions:
-    for sequence in range(no_sequence):
-        window = []
-        for frame_num in range(sequence_length):
-            res = np.load(os.path.join(Data_path, action, str(sequence), f"{frame_num}.npy"))
-            window.append(res)
-        sequences.append(window)
-        labels.append(label_map[action])
-
-x = np.array(sequences)
-y = to_categorical(labels).astype(int)
-X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.05)
-
 for action in actions:
     for sequence in range(no_sequence):
         for frame_num in range(sequence_length):
@@ -168,3 +153,37 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 # Release resources and close windows
 cap.release()
 cv2.destroyAllWindows()
+
+
+label_map = {label:num for num, label in enumerate(actions)}
+sequences, labels = [], []
+for action in actions:
+    for sequence in range(no_sequence):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(Data_path, action, str(sequence), f"{frame_num}.npy"))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+x = np.array(sequences)
+y = to_categorical(labels).astype(int)
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.05)
+
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+res = [.7, 0.2, 0.1]
+actions[np.argmax(res)]
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.fit(X_train, Y_train, epochs=2000, callbacks=[tb_callback])
+
+res = model.predict(X_test)
