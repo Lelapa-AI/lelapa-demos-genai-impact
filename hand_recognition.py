bc54535e9e41924ab266207d437e7d32266c929a
unknown <ssmagwaza88@gmail.com>
2024-08-09 13:50:55 +0200

    updated code but still need to modify feed being 2

--- hand_recognition.py
+++ hand_recognition.py
@@ -12,18 +12,19 @@ import matplotlib.pyplot as plt
 import time 
 import mediapipe as mp
 import os  
+import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import TensorBoard
+# from tensorflow.keras.utils import to_categorical
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import LSTM, Dense
+# from tensorflow.keras.callbacks. import TensorBoard
 
 # Initialize MediaPipe Holistic model and drawing utilities
 mp_holistic = mp.solutions.mediapipe.solutions.holistic
 mp_drawing = mp.solutions.mediapipe.solutions.drawing_utils
 
-Data_path = os.getcwd()
+Data_path = "data"
 actions = np.array(["hello", "thanks", "IloveYou"])
 no_sequence = 30
 sequence_length = 30
@@ -109,10 +110,10 @@ def extract_keypoints(results):
         numpy.ndarray: A flattened array of keypoints for pose, face, left hand,
         and right hand.
     """
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmarks]).flatten() if results.pose_landmarks else np.zeros(132)
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmarks]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmarks]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    face = np.array([[res.x, res.y, res.z, res.visibility] for res in results.face_landmarks.landmarks]).flatten() if results.face_landmarks else np.zeros(1404)
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    face = np.array([[res.x, res.y, res.z, res.visibility] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
     return np.concatenate([pose, face, lh, rh])
 
 # Capture video from the default camera
@@ -167,19 +168,19 @@ for action in actions:
         labels.append(label_map[action])
 
 x = np.array(sequences)
-y = to_categorical(labels).astype(int)
+y = tf.keras.utils.to_categorical(labels).astype(int)
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.05)
 
 log_dir = os.path.join('Logs')
-tb_callback = TensorBoard(log_dir=log_dir)
+tb_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
 
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu'))
-model.add(LSTM(64, return_sequences=False, activation='relu'))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(actions.shape[0], activation='softmax'))
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
+model.add(tf.keras.layers.LSTM(128, return_sequences=True, activation='relu'))
+model.add(tf.keras.layers.LSTM(64, return_sequences=False, activation='relu'))
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(32, activation='relu'))
+model.add(tf.keras.layers.Dense(actions.shape[0], activation='softmax'))
 
 res = [.7, 0.2, 0.1]
 actions[np.argmax(res)]
