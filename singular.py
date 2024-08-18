42e1cc8524d86605ae851da4c5cc43d02cda8793
unknown <ssmagwaza88@gmail.com>
2024-08-18 12:07:07 +0200

    updated code and now is working, problem now is the person needs frame awareness for proper use of the program. Their extremities, elbows, etc must be in frame

--- singular.py
+++ singular.py
@@ -173,8 +173,8 @@ class RealTimePredictor:
         self.threshold = 0.5
         self.colors = [(245,117,16), (117,245,16), (16,117,245)]
 
-    def prob_viz(self, res, colors):
-        output_frame = np.copy(res)
+    def prob_viz(self, image, res, colors):
+        output_frame = image.copy()
         for num, prob in enumerate(res):
             cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
             cv2.putText(output_frame, self.actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
@@ -209,7 +209,7 @@ class RealTimePredictor:
                 if len(self.sentence) > 5:
                     self.sentence = self.sentence[-5:]
 
-                image = self.prob_viz(res, self.colors)
+                image = self.prob_viz(image, res, self.colors)
 
             cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
             cv2.putText(image, ' '.join(self.sentence), (3, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
