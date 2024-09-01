cc006414246d3ea565da027c8d450dd842c73f6f
Siyabonga Magwaza <smagwaza023@student.wethinkcode.co.za>
2024-09-01 21:01:49 +0200

    made all the code functional instead of being commented out

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 import numpy as np
 from data_processing import DataCollector, DataPreprocessor
-# from model import ActionModel
-# from predictor import RealTimePredictor
-# from translation import translation
+from model import ActionModel
+from predictor import RealTimePredictor
+from translation import translation
 
 if __name__ == "__main__":
     # Define paths and parameters
@@ -13,29 +13,35 @@ if __name__ == "__main__":
     ACTIONS = np.array(ACTIONSS)
     NO_SEQUENCES = 30
     SEQUENCE_LENGTH = 30
-
-    # # Data Collection
-    # collector = DataCollector(DATA_PATH, ACTIONS, NO_SEQUENCES, SEQUENCE_LENGTH)
-    # collector.setup_folders()
-    # collector.collect_data()
-
-    # # Data Preprocessing
-    # preprocessor = DataPreprocessor(DATA_PATH, ACTIONS, SEQUENCE_LENGTH)
-    # X_train, X_test, y_train, y_test = preprocessor.preprocess_data()
-
-    # # Model Training
-    # model_handler = ActionModel(ACTIONS)
-    # model_handler.train_model(X_train, y_train)
-    # model_handler.save_model('action10.keras')
-
-    # # Model Evaluation
-    # model_handler.evaluate_model(X_test, y_test)
     
-    # model_handler = ActionModel(ACTIONS)
-    # model_handler.load_model(os.path.join(os.getcwd(), "action1.keras"))  # Load the model
+    determine_action = input("Are you training the model (Yes/No)").lower()
+    if determine_action[0] == "y":
+        
 
-    # # Real-Time Prediction
-    # predictor = RealTimePredictor(model_handler.model, ACTIONS)
-    # predictor.predict_in_real_time()
-    # word = predictor.get_word()
-    # translation(word[0])
+        # Data Collection
+        collector = DataCollector(DATA_PATH, ACTIONS, NO_SEQUENCES, SEQUENCE_LENGTH)
+        collector.setup_folders()
+        collector.collect_data()
+
+        # Data Preprocessing
+        preprocessor = DataPreprocessor(DATA_PATH, ACTIONS, SEQUENCE_LENGTH)
+        X_train, X_test, y_train, y_test = preprocessor.preprocess_data()
+
+        # Model Training
+        model_handler = ActionModel(ACTIONS)
+        model_handler.train_model(X_train, y_train)
+        model_handler.save_model('action10.keras')
+
+        # Model Evaluation
+        model_handler.evaluate_model(X_test, y_test)
+    
+        model_handler = ActionModel(ACTIONS)
+        model_handler.load_model(os.path.join(os.getcwd(), "action1.keras"))  # Load the model
+        
+    else:
+
+        # Real-Time Prediction
+        predictor = RealTimePredictor(model_handler.model, ACTIONS)
+        predictor.predict_in_real_time()
+        word = predictor.get_word()
+        translation(word[0])
